7fc3aca7d095651c881764ad1266a9059d911ef0
A Statistical Analysis of Fast food consumption Behaviour of Students in Tirupati <chglory8888@gmail.com>
2026-07-25 22:10:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
-// =======================
-// Start Surprise
-// =======================
+// =============================
+// Birthday Surprise Script
+// =============================
+
+
+// Start Game
 
 function startGame(){
 
-    document.getElementById("welcomeScreen")
-    .classList.add("hidden");
+document.getElementById("welcomeScreen")
+.classList.add("hidden");
 
-    document.getElementById("balloonSection")
-    .classList.remove("hidden");
 
-    createBalloons();
+document.getElementById("balloonSection")
+.classList.remove("hidden");
+
+
+startMusic();
+
+createBalloons();
 
 }
 
 
 
 
-// =======================
+
 // Background Music
-// =======================
 
 function startMusic(){
 
-    let music = document.getElementById("bgMusic");
+let music=document.getElementById("bgMusic");
 
-    if(music){
+music.volume=0.5;
 
-        music.play();
+music.play()
+.catch(()=>{
 
-    }
+console.log("Music waiting");
+
+});
 
 }
 
 
 
 
-// =======================
-// Balloons
-// =======================
 
-let balloonNumbers = [
-3,15,57,20,31,10,26,45
-];
-
+// Create Balloon Bouquet
 
 function createBalloons(){
 
@@ -52,8 +55,12 @@ let box=document.getElementById("balloonBouquet");
 box.innerHTML="";
 
 
-let numbers=[7,15,20,31,45,57,10,26];
+let numbers=[
+7,15,20,31,45,57,10,26
+];
 
+
+// create 50 balloons
 
 for(let i=0;i<50;i++){
 
@@ -63,426 +70,51 @@ let item=document.createElement("div");
 item.className="balloonItem";
 
 
+
 let balloon=document.createElement("div");
 
 balloon.className="balloon";
 
 
 
+// positions
+
+item.style.left=
+Math.random()*330+"px";
+
+
+item.style.top=
+Math.random()*300+"px";
+
+
+
+// first 8 balloons have numbers
+
 if(i<8){
 
+
 balloon.innerHTML=numbers[i];
+
 
 
 balloon.onclick=function(){
 
 
-if(numbers[i]==26){
+
+if(numbers[i]===26){
 
 
-document.getElementById("blastSound").play();
+// correct
+
+document.getElementById("blastSound")
+.play();
+
 
 
 balloon.innerHTML="💥";
 
 
-setTimeout(()=>{
-
-
-document.getElementById("balloonSection").classList.add("hidden");
-
-document.getElementById("chinnari").classList.remove("hidden");
-
-
-},700);
-
-
-}
-
-else{
-
-
-document.getElementById("wrongSound").play();
-
-}
-
-
-};
-
-
-}
-
-
-
-item.appendChild(balloon);
-
-box.appendChild(item);
-
-
-}
-
-
-}
-
-
-
-
-
-
-// =======================
-// Cake
-// =======================
-
-function showCake(){
-
-    document.getElementById("chinnari")
-    .classList.add("hidden");
-
-
-    document.getElementById("cakeSection")
-    .classList.remove("hidden");
-
-}
-
-
-
-function blowCandle(){
-
-    document.getElementById("cakeSection")
-    .classList.add("hidden");
-
-
-    document.getElementById("giftSection")
-    .classList.remove("hidden");
-
-
-}
-
-
-
-
-
-
-// =======================
-// Gift
-// =======================
-
-function openGift(){
-
-    document.getElementById("giftSection")
-    .classList.add("hidden");
-
-
-    document.getElementById("memorySection")
-    .classList.remove("hidden");
-
-
-}
-
-
-
-
-
-
-
-// =======================
-// 20 Images Slider
-// =======================
-
-
-let photos=[
-
-"IMG20250801125626.jpg",
-"IMG20250801130030.jpg",
-"IMG20250801130041.jpg",
-"IMG20250808202607.jpg",
-"IMG20250808202610.jpg",
-"IMG20250808202801.jpg",
-"IMG20250809204959.jpg",
-"IMG20250809205006.jpg",
-"IMG20250813114618.jpg",
-"IMG20250813114658.jpg",
-"IMG20250816134319.jpg",
-"IMG20251004182338.jpg",
-"IMG20251004182550.jpg",
-"IMG20251004182657.jpg",
-"IMG20251004182706.jpg",
-"IMG20251207194247.jpg",
-"Snapchat-1511237649.jpg",
-"Snapchat-974702302.jpg",
-"IMG20250809123456.jpg",
-"IMG20250810123456.jpg"
-
-];
-
-
-let photoIndex=0;
-
-
-
-setInterval(function(){
-
-
-let image =
-document.getElementById("slideImage");
-
-
-if(image){
-
-
-photoIndex++;
-
-
-if(photoIndex >= photos.length){
-
-photoIndex=0;
-
-}
-
-
-image.src=photos[photoIndex];
-
-
-}
-
-
-},3000);
-
-
-
-
-
-
-
-// =======================
-// Wishes
-// =======================
-
-function showWishes(){
-
-    document.getElementById("memorySection")
-    .classList.add("hidden");
-
-
-    document.getElementById("wishSection")
-    .classList.remove("hidden");
-
-}
-
-
-
-
-
-
-
-// =======================
-// Password
-// =======================
-
-function showPassword(){
-
-    document.getElementById("wishSection")
-    .classList.add("hidden");
-
-
-    document.getElementById("passwordSection")
-    .classList.remove("hidden");
-
-}
-
-
-
-function checkPassword(){
-
-
-let password =
-document.getElementById("password").value;
-
-
-if(password==="26-07-2020"){
-
-
-document.getElementById("passwordSection")
-.classList.add("hidden");
-
-
-document.getElementById("videoSection")
-.classList.remove("hidden");
-
-
-}
-
-else{
-
-
-document.getElementById("secretMessage")
-.innerHTML="Wrong Password ❌";
-
-
-}
-
-
-}
-
-
-
-
-
-
-
-// =======================
-// Final + Fireworks
-// =======================
-
-function showFinal(){
-
-
-document.getElementById("videoSection")
-.classList.add("hidden");
-
-
-document.getElementById("finalMessage")
-.classList.remove("hidden");
-
-
-
-let sound =
-document.getElementById("fireworkSound");
-
-
-if(sound){
-
-sound.play();
-
-}
-
-
-createFireworks();
-
-
-}
-
-
-function createFireworks(){
-
-let box=document.getElementById("fireworks");
-
-
-for(let i=0;i<80;i++){
-
-
-let spark=document.createElement("span");
-
-
-spark.className="spark";
-
-
-spark.style.left=Math.random()*100+"%";
-
-spark.style.top=Math.random()*70+"%";
-
-
-spark.style.animationDelay=
-Math.random()*2+"s";
-
-
-box.appendChild(spark);
-
-
-}
-
-        }
-setInterval(function(){
-
-let image=document.getElementById("slideImage");
-
-if(image){
-
-image.classList.add("slide-animation");
-
-
-setTimeout(()=>{
-
-photoIndex++;
-
-if(photoIndex >= photos.length){
-photoIndex=0;
-}
-
-
-image.src=photos[photoIndex];
-
-
-image.classList.remove("slide-animation");
-
-
-},1000);
-
-}
-
-},4000);
-// =======================
-// Video Music Control
-// =======================
-
-let video = document.getElementById("birthdayVideo");
-let music = document.getElementById("bgMusic");
-
-
-if(video && music){
-
-
-    // Video start - music mute
-
-    video.addEventListener("play", function(){
-
-        music.pause();
-
-    });
-
-
-
-    // Video pause - music continue
-
-    video.addEventListener("pause", function(){
-
-        music.play();
-
-    });
-
-
-
-    // Video end - music continue
-
-    video.addEventListener("ended", function(){
-
-        music.play();
-
-    });
-
-
-}
-let balloons = document.querySelectorAll(".balloon");
-
-
-balloons.forEach(balloon => {
-
-
-balloon.onclick = function(){
-
-
-let number = balloon.innerHTML;
-
-
-if(number == "26"){
-
-
-document.getElementById("blastSound").play();
-
-
 balloon.style.transform="scale(2)";
-
-balloon.style.opacity="0";
 
 
 setTimeout(()=>{
@@ -505,58 +137,283 @@ document.getElementById("chinnari")
 else{
 
 
-document.getElementById("wrongSound").play();
+// wrong
+
+document.getElementById("wrongSound")
+.play();
 
 
-balloon.innerHTML="💥";
+balloon.innerHTML="❌";
 
 
-balloon.style.opacity="0";
+}
+
+};
 
 
 }
 
 
 
+item.appendChild(balloon);
+
+box.appendChild(item);
+
+
+
 }
 
+}
+
+
+
+
+
+// Chinnari Next
+
+function showCake(){
+
+
+document.getElementById("chinnari")
+.classList.add("hidden");
+
+
+document.getElementById("cakeSection")
+.classList.remove("hidden");
+
+
+}
+
+
+
+
+
+// Candle
+
+function blowCandle(){
+
+
+document.getElementById("cakeSection")
+.classList.add("hidden");
+
+
+document.getElementById("giftSection")
+.classList.remove("hidden");
+
+
+}
+
+
+
+
+
+// Gift Open
+
+function openGift(){
+
+
+document.getElementById("giftSection")
+.classList.add("hidden");
+
+
+document.getElementById("memorySection")
+.classList.remove("hidden");
+
+
+startSlider();
+
+
+}
+
+
+
+
+
+// Photo Slider
+
+
+let photos=[
+
+
+"IMG20250801125626.jpg",
+"IMG20250801130030.jpg",
+"IMG20250801130041.jpg",
+"IMG20250808202607.jpg",
+"IMG20250808202610.jpg",
+"IMG20250808202801.jpg",
+"IMG20250809204959.jpg",
+"IMG20250809205006.jpg",
+"IMG20250813114618.jpg",
+"IMG20250813114658.jpg",
+"IMG20250816134319.jpg",
+"IMG20251004182338.jpg",
+"IMG20251004182550.jpg",
+"IMG20251004182657.jpg",
+"IMG20251004182706.jpg",
+"IMG20251207194247.jpg"
+
+];
+
+
+let photoIndex=0;
+
+
+function startSlider(){
+
+
+setInterval(()=>{
+
+
+photoIndex++;
+
+
+if(photoIndex>=photos.length){
+
+photoIndex=0;
+
+}
+
+
+
+document.getElementById("slideImage")
+.src=photos[photoIndex];
+
+
+},3000);
+
+
+
+}
+
+
+
+
+
+// Wishes
+
+
+function showWishes(){
+
+
+document.getElementById("memorySection")
+.classList.add("hidden");
+
+
+document.getElementById("wishSection")
+.classList.remove("hidden");
+
+
+}
+
+
+
+
+
+// Password
+
+
+function showPassword(){
+
+
+document.getElementById("wishSection")
+.classList.add("hidden");
+
+
+document.getElementById("passwordSection")
+.classList.remove("hidden");
+
+
+}
+
+
+
+
+function checkPassword(){
+
+
+let pass=
+document.getElementById("password").value;
+
+
+
+if(pass==="26-07-2020"){
+
+
+document.getElementById("passwordSection")
+.classList.add("hidden");
+
+
+document.getElementById("videoSection")
+.classList.remove("hidden");
+
+
+}
+
+else{
+
+
+document.getElementById("secretMessage")
+.innerHTML=
+"❌ Wrong Password Try Again";
+
+
+}
+
+
+}
+
+
+
+
+
+// Video Music Control
+
+
+let video=
+document.getElementById("birthdayVideo");
+
+
+let music=
+document.getElementById("bgMusic");
+
+
+
+if(video){
+
+
+video.addEventListener("play",()=>{
+
+music.pause();
 
 });
-function popEffect(balloon){
-
-for(let i=0;i<20;i++){
-
-let particle=document.createElement("span");
-
-particle.className="particle";
-
-
-particle.style.left=
-balloon.offsetLeft+"px";
-
-
-particle.style.top=
-balloon.offsetTop+"px";
-
-
-document.body.appendChild(particle);
 
 
 
-setTimeout(()=>{
+video.addEventListener("ended",()=>{
 
-particle.remove();
+music.play();
 
-},1000);
+});
 
 
 }
 
 
-balloon.style.transform="scale(0)";
 
-balloon.style.opacity="0";
+
+
+// Final
+
+
+function showFinal(){
+
+
+document.getElementById("videoSection")
+.classList.add("hidden");
+
+
+document.getElementById("finalMessage")
+.classList.remove("hidden");
 
 
 }
-
